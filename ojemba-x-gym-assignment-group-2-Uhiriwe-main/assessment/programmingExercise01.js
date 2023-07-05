6470c742e39d14dd562f777c2b8099f7858c7aed
olivine <olivineuhiriwe200@gmail>
2023-07-05 13:37:56 +0200

    my first exercises

--- ojemba-x-gym-assignment-group-2-Uhiriwe-main/assessment/programmingExercise01.js
+++ ojemba-x-gym-assignment-group-2-Uhiriwe-main/assessment/programmingExercise01.js
@@ -4,3 +4,11 @@
  * Write an arrow function that takes two values x and y as arguments.
  * Return true if the two values are equal and of the same type.
  */
+function num(x, y)
+{
+    var z;
+    this.getCLazy = function() {
+        if (z === undefined) z = x * y;
+        return z;
+    }
+ }
